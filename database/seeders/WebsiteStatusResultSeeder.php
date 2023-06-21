87868197533903a6c469c38db90406e602b879c0
Debdulal Baidya <ovibaidya72@gmail.com>
2023-06-21 17:28:19 +0600

    Website Status Result model, migration, seeder, factory included

---
+++ database/seeders/WebsiteStatusResultSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class WebsiteStatusResultSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
