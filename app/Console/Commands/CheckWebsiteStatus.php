5532872fac9404c220182016e17a070fb9dc5369
Debdulal Baidya <ovibaidya72@gmail.com>
2023-06-22 15:03:23 +0600

    Update Online checker command

--- app/Console/Commands/CheckWebsiteStatus.php
+++ app/Console/Commands/CheckWebsiteStatus.php
@@ -29,18 +29,32 @@ class CheckWebsiteStatus extends Command
     public function handle()
     {
         $onlineStatusChecker = new OnlineStatusCheckService();
-        $websites = Website::pluck('url')->toArray();
+        $websites = Website::get()->toArray();
         $results = [];
 
-        foreach ($onlineStatusChecker->checkAll($websites) as $result) {
+        $website_update_online = [];
+        foreach ($websites as $website) {
+            // Website Result table records
+            $online_status = $onlineStatusChecker->check($website['url']);
             $results[] = [
-                'url' => $result['website'],
-                'status' => $result['status'],
+                'url' => $website['url'],
+                'website_id' => $website['id'],
+                'status' => $online_status,
                 'check_time' => now(),
                 'created_at' => now(),
                 'updated_at' => now()
             ];
+
+            // Websites table records
+            $website_update_online[] = [
+                'id' => $website['id'],
+                'url' => $website['url'],
+                'online_status' => $online_status,
+                'last_update' => now(),
+                'last_offline' => !$online_status ? now() : $website['last_offline']
+            ];
         }
         WebsiteStatusResult::insert($results);
+        Website::upsert($website_update_online, 'id');
     }
 }
